14bea6aa84975c1dce3893483c3277b0befc5d43
Juan Romero <jdrjuan@gmail.com>
2023-07-29 18:57:04 -0300

    Make tests on ProductManager in main.js

--- desafio01/app.js
+++ desafio01/app.js
@@ -1,25 +1,46 @@
 import Product from './classes/Product.js';
+import ProductManager from './classes/ProductManager.js';
 
-const products = [];
+const ProdManager = new ProductManager();
 
-products.push(new Product(
-    'Nintendo Switch', 'Consola de videojuegos híbrida.', 350, 'switch.jpg', 'NSW001', 50
-));
-
-products.push(new Product(
+ProdManager.addProduct(new Product(
     'GoPro Hero 11 Black', 'Cámara de acción 4K.', 400, 'gopro11black.jpg', 'GP001', 20
 ));
 
-products.push(new Product(
+// Same code
+ProdManager.addProduct(new Product(
+    'GoPro Hero 11 Black mini', 'Cámara de acción mini.', 360, 'gopro11mini.jpg', 'GP001', 15
+));
+
+// description null
+ProdManager.addProduct(new Product(
+    'Nintendo Switch', undefined, 350, 'switch.jpg', 'NSW001', 50
+));
+
+ProdManager.addProduct(new Product(
     'Smartphone', 'Teléfono pantalla Full HD.', 800, 'smartphone.jpg', 'SPH002', 25
 ));
 
-products.push(new Product(
+ProdManager.addProduct(new Product(
     'Laptop', 'Laptop ultradelgada Intel Core i7.', 1200, 'laptop.jpg', 'LT003', 15
 ));
 
-products.push(new Product(
+ProdManager.addProduct(new Product(
     'Auriculares', 'Auriculares con cancelación.', 150, 'auriculares.jpg', 'AU004', 100
 ));
 
-console.log(products);
+console.table(ProdManager.getProducts());
+
+
+console.log('----- getProductById(1) -----');
+console.log(ProdManager.getProductById(1));
+
+console.log('----- getProductById(40) -----');
+console.log(ProdManager.getProductById(40));
+
+console.log('----- getProductByCode(\'GP001\') -----');
+console.log(ProdManager.getProductByCode('GP001'));
+
+console.log('----- getProductByCode(\'XXX\') -----');
+console.log(ProdManager.getProductByCode('XXX'));
+
