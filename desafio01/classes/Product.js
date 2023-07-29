d43d8cbc57bb2b2ed0968b04c749a008a0876c03
Juan Romero <jdrjuan@gmail.com>
2023-07-29 16:26:35 -0300

    Adds Product.getNextId static method

--- desafio01/classes/Product.js
+++ desafio01/classes/Product.js
@@ -9,6 +9,17 @@ class Product {
         this.stock = stock;
     }
 
+    /**
+     * Gets a new unique identifier every time this method is called.
+     * @returns {number} New unique identifier.
+     */
+    static getNewId () {
+        if (!this.nextId) {
+            this.nextId = 0;
+        }
+        return ++this.nextId;
+    }
+
 }
 
 export default Product;
