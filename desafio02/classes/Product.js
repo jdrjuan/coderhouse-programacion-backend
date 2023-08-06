3634cab953efb2ae939a48791e6ca8e178c120c9
Juan Romero <jdrjuan@gmail.com>
2023-08-06 18:14:19 -0300

    Moves the line of the id property assignment in the Product constructor

--- desafio02/classes/Product.js
+++ desafio02/classes/Product.js
@@ -1,13 +1,13 @@
 class Product {
 
     constructor(title, description, price, thumbnail, code, stock) {
+        this.id = Product.getNewId();
         this.title = title;
         this.description = description;
         this.price = price;
         this.thumbnail = thumbnail;
         this.code = code;
         this.stock = stock;
-        this.id = Product.getNewId();
     }
 
     /**
