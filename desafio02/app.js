9949cf861b70d3a64bee4cd35ee371cbd101fdf9
Juan Romero <jdrjuan@gmail.com>
2023-08-07 08:06:17 -0300

    Test all CRUD operations in app.js

--- desafio02/app.js
+++ desafio02/app.js
@@ -6,44 +6,45 @@ const PRODUCTS_FILE_PATH = config.PRODUCTS_FILE_PATH || './data/products.json';
 
 const productManager = new ProductManager(PRODUCTS_FILE_PATH);
 
-productManager.addProduct(new Product(
+console.table(await productManager.getProducts());
+
+await productManager.addProduct(new Product(
     'GoPro Hero 11 Black', 'Cámara de acción 4K.', 400, 'gopro11black.jpg', 'GP001', 20
 ));
 
+await productManager.addProduct(new Product(
+    'Galaxy S20FE', 'Excelentes prestaciones. Excelente precio.', 800, 'smartphone.jpg', 'SPH002', 25
+));
+
+await productManager.addProduct(new Product(
+    'Laptop R700', 'Laptop ultradelgada Intel Core i7.', 1200, 'laptop.jpg', 'LT003', 15
+));
+
 // Same code
-productManager.addProduct(new Product(
+await productManager.addProduct(new Product(
     'GoPro Hero 11 Black mini', 'Cámara de acción mini.', 360, 'gopro11mini.jpg', 'GP001', 15
 ));
 
 // description null
-productManager.addProduct(new Product(
+await productManager.addProduct(new Product(
     'Nintendo Switch', undefined, 350, 'switch.jpg', 'NSW001', 50
 ));
 
-productManager.addProduct(new Product(
-    'Smartphone', 'Teléfono pantalla Full HD.', 800, 'smartphone.jpg', 'SPH002', 25
-));
-
-productManager.addProduct(new Product(
-    'Laptop', 'Laptop ultradelgada Intel Core i7.', 1200, 'laptop.jpg', 'LT003', 15
-));
-
-productManager.addProduct(new Product(
+await productManager.addProduct(new Product(
     'Auriculares', 'Auriculares con cancelación.', 150, 'auriculares.jpg', 'AU004', 100
 ));
 
-console.table(productManager.getProducts());
 
+await productManager.updateProduct(4, new Product(
+    'New Title', 'New description', 599, 'new-thumbnail.jpg', 'NEW', 33
+));
 
-console.log('----- getProductById(1) -----');
-console.log(productManager.getProductById(1));
+console.log(await productManager.getProductById(10));
+console.log(await productManager.getProductById(8888));
 
-console.log('----- getProductById(40) -----');
-console.log(productManager.getProductById(40));
+console.log(await productManager.getProductByCode('GP001'));
+console.log(await productManager.getProductByCode('XXX'));
 
-console.log('----- getProductByCode(\'GP001\') -----');
-console.log(productManager.getProductByCode('GP001'));
+await productManager.deleteProduct(4);
 
-console.log('----- getProductByCode(\'XXX\') -----');
-console.log(productManager.getProductByCode('XXX'));
-
+console.table(await productManager.getProducts());
