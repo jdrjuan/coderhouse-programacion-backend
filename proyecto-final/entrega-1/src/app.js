0bf44df12da5bb4a278f7f873e78eb562ad753dd
Juan Romero <jdrjuan@gmail.com>
2023-08-18 12:48:12 -0300

    Adds products router and uses it for it's routes

--- proyecto-final/entrega-1/src/app.js
+++ proyecto-final/entrega-1/src/app.js
@@ -1,38 +1,10 @@
 import express from 'express';
-import ProductManager from './services/ProductManager.js';
 import config from './config.js';
-
-const PRODUCTS_FILE_PATH = config.PRODUCTS_FILE_PATH || './data/products.json';
-
-const productManager = new ProductManager(PRODUCTS_FILE_PATH);
+import productRouter from './routes/product.routes.js';
 
 const app = express();
 
-app.get('/products/', async (req, res) => {
-    const products = await productManager.getProducts();
-    const {limit} = req.query;
-
-    // If limit string contains an integer value
-    if(/^\d+$/.test(limit)) {
-        const productsLimit = +limit;
-        const productsRange = products.slice(0, productsLimit);
-        return res.send(productsRange);
-    }
-    
-    res.send(products);
-})
-
-app.get('/products/:pid', async (req, res) => {
-    const {pid} = req.params;
-
-    const product = await productManager.getProductById(parseInt(pid));
-    if (!product) {
-        res.status(404).send({});
-        return;
-    }
-
-    res.send(product);
-})
+app.use('/products', productRouter);
 
 const server = app.listen(config.PORT, () => { console.log(`Servidor escuchando en puerto ${config.PORT}`)});
 server.on('error', error => console.log('Error al iniciar el servidor:', error.message));
---
+++ proyecto-final/entrega-1/src/routes/product.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import ProductManager from '../services/ProductManager.js';
+import config from '../config.js';
+
+const productRouter = Router();
+
+const PRODUCTS_FILE_PATH = config.PRODUCTS_FILE_PATH || './data/products.json';
+const productManager = new ProductManager(PRODUCTS_FILE_PATH);
+
+
+productRouter.get('/', async (req, res) => {
+    const products = await productManager.getProducts();
+    const {limit} = req.query;
+
+    // If limit string contains an integer value
+    if(/^\d+$/.test(limit)) {
+        const productsLimit = +limit;
+        const productsRange = products.slice(0, productsLimit);
+        return res.send(productsRange);
+    }
+    
+    res.send(products);
+})
+
+productRouter.get('/:pid', async (req, res) => {
+    const {pid} = req.params;
+
+    const product = await productManager.getProductById(parseInt(pid));
+    if (!product) {
+        res.status(404).send({});
+        return;
+    }
+
+    res.send(product);
+})
+
+
+export default productRouter;
