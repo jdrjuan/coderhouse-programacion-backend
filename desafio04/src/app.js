3c7942b568b939062197bc292001f1e8107171fe
Juan Romero <jdrjuan@gmail.com>
2023-08-26 18:35:33 -0300

    Uses desafio03 branch as base and adjust package.json

---
+++ desafio04/src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import ProductManager from './services/ProductManager.js';
+import config from './config.js';
+
+const PRODUCTS_FILE_PATH = config.PRODUCTS_FILE_PATH || './data/products.json';
+
+const productManager = new ProductManager(PRODUCTS_FILE_PATH);
+
+const app = express();
+
+app.get('/products/', async (req, res) => {
+    const products = await productManager.getProducts();
+    const {limit} = req.query;
+
+    // If limit string contains an integer value
+    if(/^\d+$/.test(limit)) {
+        const productsLimit = +limit;
+        const productsRange = products.slice(0, productsLimit);
+        return res.send(productsRange);
+    }
+    
+    res.send(products);
+})
+
+app.get('/products/:pid', async (req, res) => {
+    const {pid} = req.params;
+
+    const product = await productManager.getProductById(parseInt(pid));
+    if (!product) {
+        res.status(404).send({});
+        return;
+    }
+
+    res.send(product);
+})
+
+const server = app.listen(config.PORT, () => { console.log(`Servidor escuchando en puerto ${config.PORT}`)});
+server.on('error', error => console.log('Error al iniciar el servidor:', error.message));
